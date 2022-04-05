64136832c6c097182ee12e3c99369defdd4d1f7a
Gustavo Martins <gustavomartins.developer@gmail.com>
2022-04-05 14:01:54 -0300

    Projeto Crud Usuarios React

---
+++ frontend/src/components/template/Footer.jsx
@@ -0,0 +1,10 @@
+import './Footer.css'
+import React from 'react'
+
+export default props =>
+    <footer className="footer">
+        <span>
+            Desenvolvido com <i className="fa fa-heart text-danger"></i>por
+            <strong> Gustavo<span className="text-danger">F</span>Martins</strong>
+        </span>
+    </footer>
